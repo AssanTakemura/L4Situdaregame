2ae2c2f106cb86a5e42becb2d62cd460d4745a70
Asuka Takemura <assan.takemura@gamil.com>
2020-09-03 12:01:22 +0900

    Initial Commit

---
+++ L4Situdaregame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L4Situdaregame
+//
+//  Created by 竹村明日香 on 2020/09/03.
+//  Copyright © 2020 Takemura assan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
